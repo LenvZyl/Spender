783db92dde777ae0c0d7db66dcd11e8de40bcb04
Lenx <lenvzyl@gmail.com>
2021-07-20 10:01:44 +0200

    Project Setup

---
+++ Spender/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spender
+//
+//  Created by Len van Zyl on 2021/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
